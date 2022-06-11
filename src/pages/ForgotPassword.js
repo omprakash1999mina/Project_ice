e770728ea4a02f2249a938e2d2853377ce6509f7
OP_Developer <op1999mina@gmail.com>
2022-06-11 23:47:16 +0530

    Forgot password added

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -72,7 +72,7 @@ const ForgotPassword = () => {
                                 </div>
                                 {processing ?
                                     <div className="flex items-center justify-center">
-                                        <button type="button" className="inline-flex items-center justify-center mt-4 px2 py-2 w-4/5 bg-gray-500 border border-white text-sm sm:text-base shadow-md font-medium tracking-wider text-white rounded-lg hover:shadow-lg hover:bg-gray-600 cursor-not-allowed" disabled="">
+                                        <button type="button" className="inline-flex items-center justify-center mt-4 px2 py-2 w-4/5 bg-gray-500 border border-white text-base shadow-md font-medium tracking-wider text-white rounded-lg hover:shadow-lg hover:bg-gray-600 cursor-not-allowed" disabled="">
                                             <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24">
                                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -81,9 +81,9 @@ const ForgotPassword = () => {
                                         </button>
                                     </div>
                                     :
-                                    <button type='submit' onClick={(e) => SendOtp(e)} className="mt-4 px2 py-2 w-4/5 bg-gray-600 border border-white text-sm sm:text-base shadow-md font-medium tracking-wider text-white rounded-lg hover:shadow-lg hover:bg-gray-700">Verify Email</button>
+                                    <button type='submit' onClick={(e) => SendOtp(e)} className="mt-4 px2 py-2 w-4/5 bg-gray-600 border border-white text-base shadow-md font-medium tracking-wider text-white rounded-lg hover:shadow-lg hover:bg-gray-700">Verify Email</button>
                                 }
-                                <Link to="/login" className="inline-flex px-8 py-2 text-sm sm:text-base text-indigo-400">Back To Login</Link>
+                                <Link to="/login" className="inline-flex px-8 py-2 text-base text-indigo-400">Back To Login</Link>
                                 {/* <p className="text-sm text-gray-500 ">If you did'nt get the email, ask to <button className={`${counter && 'cursor-not-allowed'} text-indigo-400`} >Resend</button> OTP {counter > 0 && (counter >= 10 ? `After 00:${counter}s` : `After 00:0${counter}s`)}</p> */}
                             </form>
                         </div>
