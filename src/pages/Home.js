c0eb2889214f8d66bb7888ec0922da677d823f0f
Om Prakash Bairwa <op1999mina@gmail.com>
2024-09-22 12:29:08 -0700

    Owner updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,33 +22,33 @@ const Home = (props) => {
             <Products />
         </div>
             <Contact />
-            <section ref={ref} className="text-gray-600 body-font ">
-            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                <h2 className="sm:text-3xl text-2xl font-bold uppercase title-font mb-4 text-gray-900" >Our Founder</h2>
-                <div className="mt-1 flex items-center">
-                    <span className="inline-block overflow-hidden ">
-                        <img className="object-cover" alt="Founder" src="/images/op.png" />
-                    </span>
-                </div>
-                {/* <img className="sm:w- mb-10 object-cover object-center rounded" alt="hero" src="/images/op.png"/> */}
-                <div className="text-center lg:w-2/3 w-full">
-                <h1 className="title-font sm:text-2xl text-2xl mb-4 font-medium text-gray-900">Om Prakash Bairwa</h1>
+            // <section ref={ref} className="text-gray-600 body-font ">
+            // <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            //     <h2 className="sm:text-3xl text-2xl font-bold uppercase title-font mb-4 text-gray-900" >Our Founder</h2>
+            //     <div className="mt-1 flex items-center">
+            //         <span className="inline-block overflow-hidden ">
+            //             <img className="object-cover" alt="Founder" src="/images/op.png" />
+            //         </span>
+            //     </div>
+            //     {/* <img className="sm:w- mb-10 object-cover object-center rounded" alt="hero" src="/images/op.png"/> */}
+            //     <div className="text-center lg:w-2/3 w-full">
+            //     <h1 className="title-font sm:text-2xl text-2xl mb-4 font-medium text-gray-900">Om Prakash Bairwa</h1>
                 
-                <div className='p-4'>
-                <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://github.com/omprakash1999mina" target="_blank" rel="noopener noreferrer">GitHub</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-                <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://in.linkedin.com/in/om-prakash-bairwa-a82a2b203?trk=people-guest_people_search-card" target="_blank" rel="noopener noreferrer">LinkedIn</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-                <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://www.instagram.com/royal_iitian/?r=nametag" target="_blank" rel="noopener noreferrer">Instagram</a>
+            //     <div className='p-4'>
+            //     <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://github.com/omprakash1999mina" target="_blank" rel="noopener noreferrer">GitHub</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            //     <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://in.linkedin.com/in/om-prakash-bairwa-a82a2b203?trk=people-guest_people_search-card" target="_blank" rel="noopener noreferrer">LinkedIn</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            //     <a className="text-black active:bg-blue-700 hover:bg-gray-700 hover:rounded-lg hover:text-white px-3 py-2  font-medium" href="https://www.instagram.com/royal_iitian/?r=nametag" target="_blank" rel="noopener noreferrer">Instagram</a>
 
-                </div>
-                <h4 className='text-black pb-2'>Young IITian and Web-Developer | Open-Source contributor </h4>
-                <br/>
-                <p className="mb-8 leading-relaxed">
-                    Pursuing a B.Tech + M.Tech Dual degree [ IDD ] in Electrical Engineering and a specialization in Power-Electronics udergraduate at IIT BHU, Varanasi. I'm interested in collaborate with others and build something exciting.I build some projects with team work and the experience was amazing .I have a good command on C,C++,Data-Structures and Web-Development with MERN stack. I enjoy bringing projects alive from scratch, from designing the UI, laying out the REST-API'S and developing secure, fast Databases and debugging to completion.
-                    Want to Develope something Secure Don't wait just contact me i'm here for you, collaborate with me . 
-                </p>
-                </div>
-            </div>
-            </section>
+            //     </div>
+            //     <h4 className='text-black pb-2'>Young IITian and Web-Developer | Open-Source contributor </h4>
+            //     <br/>
+            //     <p className="mb-8 leading-relaxed">
+            //         Pursuing a B.Tech + M.Tech Dual degree [ IDD ] in Electrical Engineering and a specialization in Power-Electronics udergraduate at IIT BHU, Varanasi. I'm interested in collaborate with others and build something exciting.I build some projects with team work and the experience was amazing .I have a good command on C,C++,Data-Structures and Web-Development with MERN stack. I enjoy bringing projects alive from scratch, from designing the UI, laying out the REST-API'S and developing secure, fast Databases and debugging to completion.
+            //         Want to Develope something Secure Don't wait just contact me i'm here for you, collaborate with me . 
+            //     </p>
+            //     </div>
+            // </div>
+            // </section>
            
        </section>
     )
